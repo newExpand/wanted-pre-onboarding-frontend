013946c1f6baea18f6f13511bff59c524811fd13
newExpand <magin0auto@gmail.com>
2023-04-18 00:16:20 +0900

    feat: 이메일 비밀번호 유효성 안건 완료

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form } from "react-router-dom";
 import classes from "./SignIn.module.css";
 
 interface Props {
@@ -6,18 +7,51 @@ interface Props {
 }
 
 const SignIn = (props: Props) => {
+    const [emailValid, setEmailValid] = useState("");
+    const [passwordValid, setPasswordValid] = useState("");
+
+    const emailValidHandler = (e: React.ChangeEvent) => {
+        const target = e.target as HTMLInputElement;
+        setEmailValid(target.value);
+    };
+
+    const passwordValidHandler = (e: React.ChangeEvent) => {
+        const target = e.target as HTMLInputElement;
+        setPasswordValid(target.value);
+    };
+
+    const isEmailError = !emailValid.includes("@");
+    const isPasswordError = !(passwordValid.length > 7);
+    const isEmailBlurError = !(emailValid.trim().length === 0);
+    const isPasswordBlurError = !(passwordValid.trim().length === 0);
+    const isDisabled = !isEmailError && !isPasswordError && isEmailBlurError && isPasswordBlurError;
+
     return (
         <div
             className={`${classes.formContainer} ${classes.signInContainer} ${
                 props.isActive && classes.active
             }`}
         >
-            <form>
+            <Form method="post">
                 <h1>이메일 로그인</h1>
-                <input type="email" data-testid="email-input" placeholder="Email" />
-                <input type="password" data-testid="password-input" placeholder="Password" />
-                <button data-testid="signin-button">로그인</button>
-            </form>
+                <input
+                    type="email"
+                    data-testid="email-input"
+                    placeholder="Email"
+                    onChange={emailValidHandler}
+                />
+                {isEmailError && isEmailBlurError && <p>@가 포함되어 있지 않습니다.</p>}
+                <input
+                    type="password"
+                    data-testid="password-input"
+                    placeholder="Password"
+                    onChange={passwordValidHandler}
+                />
+                {isPasswordError && isPasswordBlurError && <p>비밀번호가 8글자 미만 입니다.</p>}
+                <button disabled={isDisabled} data-testid="signin-button">
+                    로그인
+                </button>
+            </Form>
         </div>
     );
 };
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form } from "react-router-dom";
 import classes from "./SignUp.module.css";
 
 interface Props {
@@ -6,18 +7,52 @@ interface Props {
 }
 
 const SignUp = (props: Props) => {
+    const [emailValid, setEmailValid] = useState("");
+    const [passwordValid, setPasswordValid] = useState("");
+
+    const emailValidHandler = (e: React.ChangeEvent) => {
+        const target = e.target as HTMLInputElement;
+        setEmailValid(target.value);
+    };
+
+    const passwordValidHandler = (e: React.ChangeEvent) => {
+        const target = e.target as HTMLInputElement;
+        setPasswordValid(target.value);
+    };
+
+    const isEmailError = !emailValid.includes("@");
+    const isPasswordError = !(passwordValid.length > 7);
+    const isEmailBlurError = !(emailValid.trim().length === 0);
+    const isPasswordBlurError = !(passwordValid.trim().length === 0);
+    const isDisabled = !isEmailError && !isPasswordError && isEmailBlurError && isPasswordBlurError;
+
+    console.log(isEmailError, isPasswordError, isEmailBlurError, isPasswordBlurError);
     return (
         <div
             className={`${classes.formContainer} ${classes.signUpContainer} ${
                 props.isActive && classes.active
             }`}
         >
-            <form action="#">
+            <Form action="#">
                 <h1>계정 생성하기</h1>
-                <input type="email" data-testid="email-input" placeholder="Email" />
-                <input type="password" data-testid="password-input" placeholder="Password" />
-                <button data-testid="signup-button">회원가입</button>
-            </form>
+                <input
+                    type="email"
+                    data-testid="email-input"
+                    placeholder="Email"
+                    onChange={emailValidHandler}
+                />
+                {isEmailError && isEmailBlurError && <p>@가 포함되어 있지 않습니다.</p>}
+                <input
+                    type="password"
+                    data-testid="password-input"
+                    placeholder="Password"
+                    onChange={passwordValidHandler}
+                />
+                {isPasswordError && isPasswordBlurError && <p>비밀번호가 8글자 미만 입니다.</p>}
+                <button disabled={!isDisabled} data-testid="signup-button">
+                    회원가입
+                </button>
+            </Form>
         </div>
     );
 };
