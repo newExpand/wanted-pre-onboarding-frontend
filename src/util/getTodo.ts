63ade3eb708768df2237c08728a1999559150894
newExpand <magin0auto@gmail.com>
2023-04-19 04:17:41 +0900

    rename: ts로 확장자 변경

---
+++ src/util/getTodo.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const getTodo = async (token: string) => {
+    const response = await axios({
+        url: process.env.REACT_APP_TODO_API + "todos",
+        headers: { "Content-Type": "application/json", Authorization: "Bearer " + token },
+    });
+
+    const resData = response.data;
+    return resData;
+};
