08ce2811f05743eddf0846de12a982b9c4d9cbe1
newExpand <magin0auto@gmail.com>
2023-04-18 18:33:13 +0900

    remove: 더미 데이터 삭제

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -14,66 +14,6 @@ const TodoItem = () => {
                     <button data-testid="delete-button">삭제</button>
                 </div>
             </li>
-            <li className={classes.todoList}>
-                <label>
-                    <input type="checkbox" />
-                    <span>asdfasdf sadfasdfasdf</span>
-                </label>
-                <div className={classes.btnWrap}>
-                    <button data-testid="modify-button">수정</button>
-                    <button data-testid="delete-button">삭제</button>
-                </div>
-            </li>
-            <li className={classes.todoList}>
-                <label>
-                    <input type="checkbox" />
-                    <span>asdfasdf sadfasdfasdf</span>
-                </label>
-                <div className={classes.btnWrap}>
-                    <button data-testid="modify-button">수정</button>
-                    <button data-testid="delete-button">삭제</button>
-                </div>
-            </li>
-            <li className={classes.todoList}>
-                <label>
-                    <input type="checkbox" />
-                    <span>asdfasdf sadfasdfasdf</span>
-                </label>
-                <div className={classes.btnWrap}>
-                    <button data-testid="modify-button">수정</button>
-                    <button data-testid="delete-button">삭제</button>
-                </div>
-            </li>
-            <li className={classes.todoList}>
-                <label>
-                    <input type="checkbox" />
-                    <span>asdfasdf sadfasdfasdf</span>
-                </label>
-                <div className={classes.btnWrap}>
-                    <button data-testid="modify-button">수정</button>
-                    <button data-testid="delete-button">삭제</button>
-                </div>
-            </li>
-            <li className={classes.todoList}>
-                <label>
-                    <input type="checkbox" />
-                    <span>asdfasdf sadfasdfasdf</span>
-                </label>
-                <div className={classes.btnWrap}>
-                    <button data-testid="modify-button">수정</button>
-                    <button data-testid="delete-button">삭제</button>
-                </div>
-            </li>
-            <li className={classes.todoList}>
-                <label>
-                    <input type="checkbox" />
-                    <span>asdfasdf sadfasdfasdf</span>
-                </label>
-                <div className={classes.btnWrap}>
-                    <button data-testid="modify-button">수정</button>
-                    <button data-testid="delete-button">삭제</button>
-                </div>
-            </li>
         </>
     );
 };
