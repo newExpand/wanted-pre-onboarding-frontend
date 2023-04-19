d6f4652048107b6281db1f167501da9ba1bb0474
newExpand <magin0auto@gmail.com>
2023-04-19 13:20:40 +0900

    refactor: 투두리스트 추가시 인풋 초기화

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-router-dom";
 import classes from "./TodoForm.module.css";
 
 const TodoForm = () => {
+    const [inputValue, setInputValue] = useState("");
+
+    const submitHandler = () => {
+        setInputValue("");
+    };
+
+    const changeValueHandler = (e: React.ChangeEvent) => {
+        const target = e.target as HTMLInputElement;
+        setInputValue(target.value);
+    };
+
     return (
         <>
             <h1 className={classes.todoTitle}>Todo List</h1>
-            <Form method="post" className={classes.todoForm}>
-                <input data-testid="new-todo-input" name="todo" aria-label="오늘의 할 일 입력칸" required />
+            <Form onSubmit={submitHandler} method="post" className={classes.todoForm}>
+                <input
+                    data-testid="new-todo-input"
+                    name="todo"
+                    value={inputValue}
+                    onChange={changeValueHandler}
+                    aria-label="오늘의 할 일 입력칸"
+                    required
+                />
                 <button data-testid="new-todo-add-button">추가</button>
             </Form>
         </>
